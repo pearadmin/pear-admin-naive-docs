a7eb71864b44514adc78a409615707765b39e075
落小梅 <425605679@qq.com>
2021-12-23 17:23:06 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * author: Jobin
+ * Email: 425605679@qq.com
+ * Desc:
+ * version: 1.0.0
+ */
+module.exports = {
+  base: '/',
+  title: 'Pear Admin Naive',
+  description: 'docs',
+  lang: 'zh-CN',
+  port: 5000,
+  markdown: {
+    lineNumbers: true
+  }
+}
